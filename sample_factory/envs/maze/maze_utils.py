c9467c075f66020a2e197d738201e8e229fb359b
Garytoner <107924748+Garytoner@users.noreply.github.com>
2023-02-09 11:32:24 +0800

    Update maze_utils.py

--- sample_factory/envs/maze/maze_utils.py
+++ sample_factory/envs/maze/maze_utils.py
@@ -3,6 +3,5 @@ import gym
 # noinspection PyUnusedLocal
 def make_maze_env(env_name, cfg=None, **kwargs):
     env = gym.make(env_name)
-    env = gym.wrapper.RecordEpisodeStatistics(env)
     return env
 
